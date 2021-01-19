b7e2e38f826dad899edc152851d7e70bd8dd797f
keyboardrapper <keyboardrapper@gmail.com>
2021-01-19 19:50:23 +0800

    111

--- main.go
+++ main.go
@@ -2,77 +2,20 @@ package main
 
 import (
 	_ "net/http/pprof"
+	"salfutil/test"
+	"time"
 )
 
 func init() {
-	//fmt.Println("初始化package main init()")
-	//e := sys.SysInit(
-	//	conf.Server.ReleaseTag,
-	//	conf.Server.ProdMode,
-	//	conf.Server.RedisAddr,
-	//	conf.Server.DBName,
-	//	conf.Server.RedisPwd,
-	//	conf.Server.LogPath,
-	//	"hall",
-	//	conf.Server.LogFileSize,
-	//	conf.Server.LogFileCount,
-	//	conf.Server.MongoIp,
-	//	conf.Server.MongoLogIp,
-	//	conf.Server.DBName,
-	//	[]string{tableName.DBT_T_PAYBASEDETAILS, tableName.DBT_T_USER})
-	////初始化系统
-	//if e != nil {
-	//	fmt.Printf("初始化的时候出错:%v", e)
-	//	os.Exit(-1)
-	//}
-	//
-	////初始化微信支付回调
-	//service.DoInitWxpay(conf.Server.WXPAY_NOTIFYURL)
-	//
-	////初始化灰度的白名单
-	//model.GetGrayReleaseUsers()
+
 }
 
 func main() {
-	////打印日志
-	//lconf.LogLevel = conf.Server.LogLevel
-	//lconf.LogPath = conf.Server.LogPath
-	//lconf.ConsolePort = conf.Server.ConsolePort
-	//
-	////微信支付配置
-	//service.WXConfig.ApiKey = conf.Server.WxApikey
-	//service.WXConfig.MchId = conf.Server.WxMchid
-	//service.WXConfig.AppId = conf.Server.WxAppid
-	//
-	////启动rpc监听
-	//_, _ = rpc_handler.LisenAndServeHallRPC(conf.Server.HallRpcAddr)
-	////初始化rpc客户端
-	//_ = rpcService.ZzHzPool.Init(conf.Server.MjZzHzRpcAddr, 1)
-	//_ = rpcService.PdkPool.Init(conf.Server.PdkRpcAddr, 1)
-	//_ = rpcService.SjhMjPool.Init(conf.Server.SjhRpcAddr, 1)
-	//_ = rpcService.NiuniuPool.Init(conf.Server.NiuRpcAddr, 1)
-	//_ = rpcService.SiChuanPool.Init(conf.Server.MjSiChuanRpcAddr, 1)
-	//_ = rpcService.ZhadanPool.Init(conf.Server.ZhadanRpcAddr, 1)
-	////_ = rpcService.LiuZhouPool.Init(conf.Server.MjLiuZhouRpcAddr, 1)
-	//_ = rpcService.PaoHuZiPool.Init(conf.Server.PaoHuZiRpcAddr, 1)
-	////_ = rpcService.PaoHuZiPool.Init(conf.Server.PaoHuZiSYRpcAddr, 1)
-	//_ = rpcService.DaTongZiPool.Init(conf.Server.DaTongZiRpcAddr,1)
-	//
-	////启动微信支付回调
-	//go func() {
-	//	wxpay.WxpayCb()
-	//}()
-	//
-	////启动调试端口
-	//go func() {
-	//	log.T("尝试开启pprof监听调试端口[23801]")
-	//	log.T("ListenAndServe:23801 [%v]", http.ListenAndServe("0.0.0.0:23801", nil))
-	//}()
-	//
-	//leaf.Run(
-	//	game.Module,
-	//	gate.Module,
-	//	login.Module,
-	//	//rpc.Module,
-	//)
+	i := 0
+	for i < 100 {
+		test.RedPackage(10, 100)
+		time.Sleep(2111)
+		i++
+	}
+
 }
--- test/test1.go
+++ test/test1.go
@@ -1,12 +1,86 @@
 package test
 
-import "casino_common/proto/ddproto"
+import (
+	"casino_common/proto/ddproto"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-func slice()  {
-	var array = [3]int32{1,2,3}
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func slice() {
+	var array = [3]int32{1, 2, 3}
 	//fmt.Print(reflect.TypeOf(array).String())
 	arr := array[0:]
-	_ = append(arr,arr...)
+	_ = append(arr, arr...)
 	var a *ddproto.GroupMemberInfo = nil
-	_ := a.GetUid()
+	_ = a.GetUid()
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		RedPackage(10, 500)
+		fmt.Println("")
+	}
+}
+// 随机红包// remainCount: 剩余红包数// remainMoney: 剩余红包金额（单位：分)
+func randomMoney(remainCount, remainMoney int) int {
+	if remainCount == 1 {
+		return remainMoney
+	}
+
+	//rand.Seed(time.Now().UnixNano())
+	var min = 1
+	//保证每人都有1分
+	if remainMoney == remainCount {
+		return min
+	}
+	remainMoney = remainMoney - remainCount * min
+	//最多能拿到  剩下每人平均数的2倍
+	max := remainMoney / remainCount * 2
+	money := rand.Intn(max) + min
+	return money
+}
+// 发红包// count: 红包数量// money: 红包金额（单位：分)
+func RedPackage(count, money int) {
+	for i := 0; i < count; i++ {
+		m := randomMoney(count-i, money)
+		fmt.Printf("%d  ", m)
+		if m == 0 {
+			fmt.Println("buxing")
+		}
+		money -= m
+	}
+	fmt.Println(" ")
+}
+
+
+//红包
+type RedPacket struct {
+	Rid				int32	//红包id，区分不同的红包,用redis做自增
+	userid			uint32	//发起人
+	GroupId			int32	//群id
+	CreateTime		int64	//创建时间
+	LiveTime		int64	//红包存活时间，防止后续要求时间可选
+	Count			int32 	//红包总数
+	Score			int32	//红包总分
+	RemainCount		int32	//剩余个数
+	RemainScore		int32	//剩余分，未抢光时,该字段同时可以表示返还的分数
+	BestScore 		int32	//手气最佳分数  防止要先加上
+	BestUserid		uint32	//手气最佳用户  防止要先加上
+	ScoreList		[]*GrabRedPacketInfo //每个人的领取信息
+	Status			int32	//状态(1.开抢中  2.已抢光  3.时间结束未抢光,剩余积分返还)
+}
+
+
+//抢红包信息的回复
+type GrabRedPacketInfo struct {
+	Rid				int32	//红包id，区分不同的红包
+	userid			uint32	//用户
+	GroupId			int32	//群id
+	Time			string	//时间
+	Score			int64	//抢到的分数
 }
